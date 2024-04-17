1922af117964f23ad8fcfa83fbe0bcae4d449a11
DiyatulshaNN <119120102+DiyatulshaNN@users.noreply.github.com>
2024-04-17 22:26:41 +0530

    Update assssment.cpp

--- assssment.cpp
+++ assssment.cpp
@@ -2,27 +2,32 @@
 using namespace std;
 
 bool solve( vector<int> &arr, int p, int a, int b){
-    if(a>=11) 
+    if(a>=11) //Base case if Abhimanyu has enough skips to pass all the circles
     return true;
     int cur_p=p, skp=a,enrg=b;
     for(int i=0;i<11;i++){
+        //k3 and k7 power regerate case
         if(i==3||i==7){
            arr[i]+=(arr[i-1]/2);
         }
-        
+        //If Abhimanyu has less power than enemy
         if(cur_p<arr[i]){
+            //If Abhimanyu can recharge 
           if(cur_p!=p&&p>=arr[i]&&enrg>0){
               enrg--;
               cur_p=p;
               cur_p-=arr[i];
           }
+              //if abhimanyu can skip
           else if(skp>0){
               skp--;
           }
+              //If Abhimanyu cannot defeat
           else{
             return false;
           }
         }
+            //If Abhimanyu has more power than the enemy
         else{
            cur_p-=arr[i];
         }
@@ -45,3 +50,5 @@ int main() {
 
     return 0;
 }
+// Time Complexity: O(1)
+//Space Complexity: O(1)
